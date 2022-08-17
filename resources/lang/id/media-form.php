0fac36fb826911ae9acb316c31cf42578f4b5ef0
rama-adi <17980909+rama-adi@users.noreply.github.com>
2022-08-17 22:02:05 +0700

    translate to Indonesian language

---
+++ resources/lang/id/media-form.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'labels' => [
+        'file' => 'Berkas',
+        'alt' => 'Teks Alternatif',
+        'title' => 'Judul',
+        'caption' => 'Keterangan',
+        'description' => 'Deskripsi',
+    ],
+
+    'notices' => [
+        'success' => 'Item behasil diperbarui.'
+    ]
+];
---
+++ resources/lang/id/media-picker-modal.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'heading' => 'Pemilih Media',
+    'search_label' => 'Cari',
+    'upload_tab' => 'Unggah Media',
+    'media_library_tab' => 'Pustaka Media',
+    'deselect' => 'Batalkan Pilihan',
+    'load_more' => 'Muat Lebih Banyak',
+    'empty_results' => 'Tidak ada berkas di pustaka atau hasil pencarian tidak ditemukan.',
+    'edit_media' => 'Sunting Media',
+    'edit_save' => 'Simpan',
+    'edit_delete' => 'Hapus',
+    'edit_cancel' => 'Batalkan',
+    'use_selected_image' => 'Gunakan Gambar Yang Dipilih',
+];
