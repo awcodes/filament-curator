87427c81be7dec4cd532cac3f4ee8820c1ff2bc3
awcodes <adam@adamwgd.com>
2022-08-24 11:46:06 -0400

    change overflow-scroll to overflow-auto to possibly fix persistant scrollbars as reported by users

--- resources/views/alt-helper.blade.php
+++ resources/views/alt-helper.blade.php
@@ -6,6 +6,6 @@
         href="https://www.w3.org/WAI/tutorials/images/decision-tree"
         target="_blank"
         rel="noopener"
-        class="underline"
+        class="underline text-primary-500 hover:text-primary-400 focus:text-primary-400"
     >{{ __('filament-curator::alt-helper.link') }}</a>. {{ __('filament-curator::alt-helper.text') }}
 </p>
