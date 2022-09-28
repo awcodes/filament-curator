fc6b9e34b699fd6cf7f8bbb4166fe7ce58b88b9b
awcodes <adam@adamwgd.com>
2022-09-28 13:45:03 -0400

    add CuratorColumn

---
+++ src/Tables/Columns/CuratorColumn.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace FilamentCurator\Tables\Columns;
+
+use Filament\Tables\Columns\ImageColumn;
+
+class CuratorColumn extends ImageColumn
+{
+    protected string $view = 'filament-curator::tables.columns.curator-column';
+
+    public function getImagePath(): ?string
+    {
+        $state = $this->record->image;
+
+        if (! $state) {
+            return null;
+        }
+
+        /** @var FilesystemAdapter $storage */
+        $storage = $this->getDisk();
+
+        if (! $storage->exists($state->filename)) {
+            return null;
+        }
+
+        if ($this->getVisibility() === 'private' || $storage->getVisibility($state->filename) === 'private') {
+            try {
+                return $storage->temporaryUrl(
+                    $state->filename,
+                    now()->addMinutes(5),
+                );
+            } catch (Throwable $exception) {
+                // This driver does not support creating temporary URLs.
+            }
+        }
+
+        return $state->thumbnail_url;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->record->image ? $this->record->image->type : '';
+    }
+}
