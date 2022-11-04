01c38a67d9d40475639628aa642bd2ccbcc71e94
awcodes <adam@adamwgd.com>
2022-11-04 11:23:41 -0400

    remove color from alt-helper link, let user style it

--- resources/views/alt-helper.blade.php
+++ resources/views/alt-helper.blade.php
@@ -6,6 +6,6 @@
         href="https://www.w3.org/WAI/tutorials/images/decision-tree"
         target="_blank"
         rel="noopener"
-        class="underline text-primary-500 hover:text-primary-400 focus:text-primary-400"
+        class="filament-link underline"
     >{{ __('filament-curator::alt-helper.link') }}</a>. {{ __('filament-curator::alt-helper.text') }}
 </p>
