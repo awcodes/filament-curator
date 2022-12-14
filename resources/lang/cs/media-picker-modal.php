9cc11044a4b276a4056586686d3e918686668d58
Jarka Pachlová <jarka.p@gmail.com>
2022-12-14 17:12:40 +0100

    add czech translation

---
+++ resources/lang/cs/media-picker-modal.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'heading' => 'Výběr mediálních souborů',
+    'search_label' => 'Hledat',
+    'search_placeholder' => 'Hledat',
+    'upload_tab' => 'Nahrát',
+    'media_library_tab' => 'Knihovna médií',
+    'deselect' => 'Odoznačit',
+    'load_more' => 'Načíst další',
+    'empty_results' => 'Pro zadaný výraz nebyly nalezeny žádné výsledky.',
+    'edit_media' => 'Upravit mediální soubor',
+    'edit_save' => 'Uložit',
+    'edit_delete' => 'Odstanit',
+    'edit_cancel' => 'Zrušit',
+    'use_selected_image' => 'Použít vybraný obrázek',
+    'add_files' => 'Přidat soubory',
+    'view' => 'Zobrazit',
+    'edit' => 'Upravit',
+    'download' => 'Stáhnout',
+    'remove' => 'Odstranit',
+];
