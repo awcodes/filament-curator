24924cd1eff030056102a59d4780b153ebe1f315
Marga Rizaldi <margarizaldi@gmail.com>
2023-01-31 11:17:09 +0700

    Add Indonesian (id) translations

---
+++ resources/lang/id/forms.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'sections' => [
+        'details' => 'Detail',
+        'exif' => 'Data Gambar',
+        'file' => 'Berkas',
+        'meta' => 'Meta',
+        'preview' => 'Pratinjau',
+        'upload_new' => 'Unggah Berkas Baru',
+        'curation' => 'Kurasi',
+    ],
+    'fields' => [
+        'alt' => 'Teks Alternatif (Alt)',
+        'alt_hint' => 'Alt?',
+        'caption' => 'Keterangan/Caption',
+        'description' => 'Deskripsi',
+        'file' => 'Berkas',
+        'title' => 'Judul',
+        'name' => 'Nama',
+    ],
+    'notices' => [
+        'success' => 'Berkas berhasil diperbarui.',
+    ],
+    'curations' => [
+        'button_label' => 'Buat Kurasi'
+    ]
+];
