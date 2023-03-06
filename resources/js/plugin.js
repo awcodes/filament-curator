4bbb2409bc59a6ffdfcc9fbc56b2ecc656d6990a
awcodes <adam@adamwgd.com>
2023-03-06 11:27:25 -0500

    Chore: move picker modal to dedicated Alpine component

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -1,6 +1,78 @@
 import Cropper from 'cropperjs'
 
 document.addEventListener("alpine:init", () => {
+    Alpine.data('curator', ({ statePath }) => ({
+        statePath,
+        selected: null,
+        files: [],
+        nextPageUrl: null,
+        isFetching: false,
+        init() {
+            this.getFiles('/curator/media');
+            const observer = new IntersectionObserver(
+                ([e]) => {
+                    if (e.isIntersecting) {
+                        this.loadMoreFiles();
+                    }
+                },
+                {
+                    rootMargin: '0px',
+                    threshold: [0],
+                }
+            );
+            observer.observe(this.$refs.loadMore);
+        },
+        getFiles: async function(url = '/curator/media', selected = null) {
+            if (selected) {
+                let indicator = url.includes('?') ? '&' : '?';
+                url = url + indicator + 'media_id=' + selected;
+            }
+            this.isFetching = true;
+            const response = await fetch(url);
+            const result = await response.json();
+            this.files = this.files ? this.files.concat(result.data) : result.data;
+            this.nextPageUrl = result.next_page_url;
+            this.isFetching = false;
+        },
+        loadMoreFiles: async function() {
+            if (this.nextPageUrl) {
+                this.isFetching = true;
+                await this.getFiles(this.nextPageUrl, this.selected?.id);
+                this.isFetching = false;
+            }
+        },
+        searchFiles: async function(event) {
+            this.isFetching = true;
+            const response = await fetch('/curator/media/search?q=' + event.target.value);
+            const result = await response.json();
+            this.files = result.data;
+            this.isFetching = false;
+        },
+        addNewFile: function(media = null) {
+            if (media) {
+                this.files = [...media, ...this.files];
+                this.$nextTick(() => {
+                    this.setSelected(media[0].id);
+                })
+            }
+        },
+        removeFile: function(media = null) {
+            if (media) {
+                this.files = this.files.filter((obj) => obj.id !== media.id);
+                this.selected = null;
+            }
+        },
+        setSelected: function(mediaId = null) {
+            if (!mediaId || (this.selected && this.selected.id === mediaId)) {
+                this.selected = null;
+            } else {
+                this.selected = this.files.find(obj => obj.id === mediaId);
+            }
+
+            this.$wire.setCurrentFile(this.selected);
+        },
+    }));
+
     Alpine.data('curation', ({ statePath, fileName, fileType, presets = {}}) => ({
         statePath: statePath,
         filename: fileName,
