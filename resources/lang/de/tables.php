8adea5ed45ad77e31f826ce6dfc5a7a93db432f4
Martin Mildner <martin@foxship.com>
2023-03-14 17:42:45 +0100

    Added German Translation

---
+++ resources/lang/de/tables.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'columns' => [
+        'url' => 'URL',
+        'name' => 'Name',
+        'ext' => 'Extension',
+        'disk' => 'Disk',
+        'directory' => 'Verzeichnis',
+        'created_at' => 'Erstellt am',
+    ],
+    'actions' => [
+        'toggle_table_grid' => 'Grid Ansicht',
+        'toggle_table_list' => 'Listen Ansicht',
+    ],
+];
