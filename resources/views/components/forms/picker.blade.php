bfcd7a0eac4378fc8e5cd1132f3bf669227c2919
awcodes <adam@adamwgd.com>
2023-05-15 16:25:15 -0400

    wip: still working on picker layout

--- resources/views/components/forms/picker.blade.php
+++ resources/views/components/forms/picker.blade.php
@@ -29,15 +29,19 @@
     >
 
         <div
-            class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
+            @class([
+                'flex items-center gap-6 flex-wrap' => count($items) <= 3,
+                'curator-grid-container' => count($items) >= 3,
+            ])
             wire:sortable
             wire:end.stop="dispatchFormEvent('picker::moveItems', '{{ $statePath }}', $event.target.sortable.toArray())"
+            style="{{ count($items) === 1 ? '--grid-column-count: 1' : '' }}"
         >
             @foreach ($items as $uuid => $item)
                 <div {{ $attributes->merge($getExtraAttributes())->class([
                     'relative block w-full overflow-hidden transition duration-75 border border-gray-300 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 dark:text-white flex justify-center checkered',
                     'h-64' => ! str($item['type'])->contains('video'),
-                    'md:col-span-2 lg:col-span-3 xl:col-span-4' => count($items) === 1,
+                    'md:flex-1 ' => count($items) <= 3,
                 ]) }}
                     wire:sortable.item="{{ $uuid }}"
                 >
@@ -61,61 +65,82 @@
                         />
                     @endif
 
-                    <div class="absolute top-0 right-0 flex bg-gray-900 divide-x divide-gray-700 rounded-bl-lg shadow-md">
+                    <div class="absolute top-0 right-0">
+                        <div class="relative flex items-center bg-gray-900 divide-x divide-gray-700 rounded-bl-lg shadow-md">
                         @if (count($items) > 1)
-                        <button
-                            title="{{ __('forms::components.repeater.buttons.move_item.label') }}"
-                            x-on:click.stop
-                            wire:sortable.handle
-                            type="button"
-                            class="flex items-center justify-center flex-none w-10 h-10 transition text-gray-600 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400 cursor-grab"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m22.67 12l-4.49 4.5l-2.51-2.5l1.98-2l-1.98-1.96l2.51-2.51L22.67 12M12 1.33l4.47 4.49l-2.51 2.51L12 6.35l-2 1.98l-2.5-2.51L12 1.33m0 21.34l-4.47-4.49l2.51-2.51L12 17.65l2-1.98l2.5 2.51l-4.5 4.49M1.33 12l4.49-4.5L8.33 10l-1.98 2l1.98 1.96l-2.51 2.51L1.33 12M12 10a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2Z"/></svg>
-                            <span class="sr-only">
-                                {{ __('forms::components.repeater.buttons.move_item.label') }}
-                            </span>
-                        </button>
+                            <button
+                                title="{{ __('forms::components.repeater.buttons.move_item.label') }}"
+                                x-on:click.stop
+                                x-tooltip.raw="{{ __('curator::views.picker.reorder') }}"
+                                wire:sortable.handle
+                                type="button"
+                                class="flex items-center justify-center flex-none w-10 h-10 transition text-gray-600 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400 cursor-grab"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m22.67 12l-4.49 4.5l-2.51-2.5l1.98-2l-1.98-1.96l2.51-2.51L22.67 12M12 1.33l4.47 4.49l-2.51 2.51L12 6.35l-2 1.98l-2.5-2.51L12 1.33m0 21.34l-4.47-4.49l2.51-2.51L12 17.65l2-1.98l2.5 2.51l-4.5 4.49M1.33 12l4.49-4.5L8.33 10l-1.98 2l1.98 1.96l-2.51 2.51L1.33 12M12 10a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2Z"/></svg>
+                                <span class="sr-only">
+                                    {{ __('forms::components.repeater.buttons.move_item.label') }}
+                                </span>
+                            </button>
                         @endif
-                        <a
-                            href="{{ $item['url'] }}"
-                            target="_blank"
-                            rel="noopener nofollow"
-                            class="flex items-center justify-center flex-none w-10 h-10 transition text-gray-600 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400"
-                            x-tooltip.raw="{{ __('curator::views.picker.view') }}"
-                        >
-                            @svg('heroicon-s-eye', 'w-4 h-4')
-                            <span class="sr-only">{{ __('curator::views.picker.view') }}</span>
-                        </a>
-                        <button
-                            type="button"
-                            wire:click="mountFormComponentAction('{{ $statePath }}', 'curator_download')"
-                            class="flex items-center justify-center flex-none w-10 h-10 transition text-primary-600 hover:text-primary-500 dark:text-primary-500 dark:hover:text-primary-400"
-                            x-tooltip.raw="{{ __('curator::views.picker.download') }}"
-                        >
-                            @svg('heroicon-s-download', 'w-4 h-4')
-                            <span class="sr-only">{{ __('curator::views.picker.download') }}</span>
-                        </button>
+
+                        <x-filament::dropdown placement="bottom-end" :teleport="true">
+                            <x-slot name="trigger">
+                                <button
+                                    title="{{ __('forms::components.repeater.buttons.item_options.label') }}"
+                                    type="button"
+                                    class="flex items-center justify-center flex-none w-10 h-10 transition text-gray-600 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400"
+                                >
+                                    @svg('heroicon-s-dots-vertical', 'w-4 h-4')
+                                    <span class="sr-only">
+                                        {{ __('forms::components.repeater.buttons.item_options.label') }}
+                                    </span>
+                                </button>
+                            </x-slot>
+
+                            <x-filament::dropdown.list>
+                                <x-filament::dropdown.list.item
+                                    color="secondary"
+                                    icon="heroicon-s-eye"
+                                    href="{{ $item['url'] }}"
+                                    tag="a"
+                                >
+                                    {{ __('curator::views.picker.view') }}
+                                </x-filament::dropdown.list.item>
+
+                                <x-filament-support::dropdown.list.item
+                                    color="secondary"
+                                    icon="heroicon-s-download"
+                                    wire:click="mountFormComponentAction('{{ $statePath }}', 'curator_download')"
+                                >
+                                    {{ __('curator::views.picker.download') }}
+                                </x-filament-support::dropdown.list.item>
+
+                                @if (! $isDisabled())
+                                <x-filament::dropdown.list.item
+                                    color="secondary"
+                                    icon="heroicon-s-pencil"
+                                    href="{{ \Awcodes\Curator\Resources\MediaResource::getUrl('edit', $item['id']) }}"
+                                    tag="a"
+                                >
+                                    {{ __('curator::views.picker.edit') }}
+                                </x-filament::dropdown.list.item>
+                                @endif
+                            </x-filament::dropdown.list>
+                        </x-filament::dropdown>
+
                         @if (! $isDisabled())
-                        <a
-                            href="{{ \Awcodes\Curator\Resources\MediaResource::getUrl('edit', $item['id']) }}"
-                            target="_blank"
-                            rel="noopener nofollow"
-                            class="flex items-center justify-center flex-none w-10 h-10 transition text-success-600 hover:text-success-500 dark:text-success-500 dark:hover:text-success-400"
-                            x-tooltip.raw="{{ __('curator::views.picker.edit') }}"
-                        >
-                            @svg('heroicon-s-pencil', 'w-4 h-4')
-                            <span class="sr-only">{{ __('curator::views.picker.edit') }}</span>
-                        </a>
-                        <button
-                            type="button"
-                            x-on:click="removeItem('{{ $uuid }}')"
-                            class="flex items-center justify-center flex-none w-10 h-10 transition text-danger-600 hover:text-danger-500 dark:text-danger-500 dark:hover:text-danger-400"
-                            x-tooltip.raw="{{ __('curator::views.picker.remove') }}"
-                        >
-                            @svg('heroicon-s-minus-circle', 'w-4 h-4')
-                            <span class="sr-only">{{ __('curator::views.picker.remove') }}</span>
-                        </button>
+                            <button
+                                type="button"
+                                x-on:click="removeItem('{{ $uuid }}')"
+                                x-tooltip.raw="{{ __('curator::views.picker.remove') }}"
+                                class="flex items-center justify-center flex-none w-10 h-10 transition text-danger-600 hover:text-danger-500 dark:text-danger-500 dark:hover:text-danger-400"
+                            >
+                                @svg('heroicon-s-minus-circle', 'w-4 h-4')
+                                <span class="sr-only">{{ __('curator::views.picker.remove') }}</span>
+                            </button>
                         @endif
+                        </div>
+
                     </div>
 
                     @if (! str($item['type'])->contains('video'))
