363145f99939e4ed6004ddbc3fbe6f4f228be2d3
awcodes <awcodes1@gmail.com>
2023-08-02 13:20:32 -0400

    Feat: Better Media factory

--- database/factories/MediaFactory.php
+++ database/factories/MediaFactory.php
@@ -4,6 +4,8 @@ namespace Awcodes\Curator\Database\Factories;
 
 use Awcodes\Curator\Facades\Curator;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class MediaFactory extends Factory
 {
@@ -12,46 +14,57 @@ class MediaFactory extends Factory
         return Curator::getMediaModel();
     }
 
-    public function asDocument(): Factory
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'name' => 'document.pdf',
-                'path' => 'media/document.pdf',
-                'type' => 'application/pdf',
-                'width' => null,
-                'height' => null,
-                'size' => 1024,
-            ];
-        });
-    }
-
-    public function vertical(): Factory
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'name' => 'vertical.jpg',
-                'path' => 'media/vertical.jpg',
-            ];
-        });
-    }
-
     public function definition(): array
     {
+        $fileName = collect([
+            'alberto-restifo-Ni4NgA64TFQ-unsplash',
+            'blake-verdoorn-cssvEZacHvQ-unsplash',
+            'daniel-roe-lpjb_UMOyx8-unsplash',
+            'dave-hoefler-lsoogGC_5dg-unsplash',
+            'david-marcu-78A265wPiO4-unsplash',
+            'dawid-zawila--G3rw6Y02D0-unsplash',
+            'eberhard-grossgasteiger-pBgnT4KH8d4-unsplash',
+            'eberhard-grossgasteiger-y2azHvupCVo-unsplash',
+            'fabian-quintero-UWQP2mh5YJI-unsplash',
+            'federico-respini-sYffw0LNr7s-unsplash',
+            'felix-mittermeier-L4-16dmZ-1c-unsplash',
+            'guillaume-briard-QegnXyECDfw-unsplash',
+            'henry-be-IicyiaPYGGI-unsplash',
+            'igor-kasalovic-tNDvFkxkBHo-unsplash',
+            'joel-vodell-TApAkERW5pQ-unsplash',
+            'kees-streefkerk-Adl90-aXYwA-unsplash',
+            'luca-bravo-zAjdgNXsMeg-unsplash',
+            'lukasz-szmigiel-jFCViYFYcus-unsplash',
+            'niko-photos-tGTVxeOr_Rs-unsplash',
+            'robert-lukeman-_RBcxo9AU-U-unsplash',
+            'robert-lukeman-zNN6ubHmruI-unsplash',
+            'tim-swaan-eOpewngf68w-unsplash',
+        ])->random() . '.jpg';
+
+        $directory = app('curator')->getDirectory();
+        $disk = app('curator')->getDiskName();
+
+        if (! Storage::disk($disk)->exists($directory . '/' . $fileName)) {
+            $fileContents = file_get_contents('https://res.cloudinary.com/aw-codes/image/upload/curator/seed-data/' . $fileName);
+            Storage::disk($disk)->put($directory . '/' . $fileName, $fileContents);
+        }
+
+        $data = Image::make(Storage::disk($disk)->path($directory . '/' . $fileName));
+
         return [
-            'disk' => 'public',
-            'directory' => 'media',
-            'name' => 'horizontal.jpg',
-            'path' => 'media/horizontal.jpg',
-            'width' => 640,
-            'height' => 640,
-            'size' => 540,
-            'type' => 'image',
-            'ext' => 'jpg',
-            'alt' => $this->faker->words(5, true),
-            'title' => $this->faker->words(10, true),
-            'description' => $this->faker->words(10, true),
-            'caption' => $this->faker->words(10, true),
+            'name' => $data->filename,
+            'path' => $directory . '/' . $fileName,
+            'ext' => $data->extension,
+            'type' => $data->mime(),
+            'alt' => $this->faker->words(rand(3, 8), true),
+            'title' => null,
+            'caption' => null,
+            'description' => null,
+            'width' => $data->getWidth() ?? null,
+            'height' => $data->getHeight() ?? null,
+            'disk' => $disk,
+            'directory' => $directory,
+            'size' => $data->filesize() ?? null,
         ];
     }
 }
