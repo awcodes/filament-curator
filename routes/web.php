0dcbcc69c4787ffe33f876e554513dbfdab1938e
Martin Ro <martin-ro@users.noreply.github.com>
2023-08-07 10:17:02 +0800

    Fixes Method Filament\Panel::getDomain does not exist.
    The change is introduced here: https://github.com/filamentphp/filament/pull/7398
    
    Fixes also https://github.com/awcodes/filament-curator/issues/259

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,16 @@ use Illuminate\Support\Facades\Route;
 $panel = Filament::getCurrentPanel();
 
 if ($panel) {
-    Route::domain($panel->getDomain())
-        ->middleware($panel->getMiddleware())
-        ->group(function () {
-            Route::get('/curator/media', [MediaController::class, 'index']);
-            Route::get('/curator/media/search', [MediaController::class, 'search']);
-        });
+    $domains = $panel->getDomains();
+    
+    foreach ((empty($domains) ? [null] : $domains) as $domain) {
+        Route::domain($domain)
+            ->middleware($panel->getMiddleware())
+            ->group(function () {
+                Route::get('/curator/media', [MediaController::class, 'index']);
+                Route::get('/curator/media/search', [MediaController::class, 'search']);
+            });
+    }
 }
 
 Route::get('/curator/{path}', [MediaController::class, 'show'])
