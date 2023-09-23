a6909f4e8b11ddd2d80980cab25a70f8cf3a842f
kuragehime <kuragehime641@gmail.com>
2023-09-24 00:12:33 +0900

    Create notifications.php

---
+++ resources/lang/ja/notifications.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'update_success' => 'レコードは正常に更新されました。',
+    'update_error' => 'レコードの更新中にエラーが発生しました。',
+    'delete_success' => 'レコードは正常に削除されました。',
+    'delete_error' => 'Tレコードの削除中にエラーが発生しました。',
+];
