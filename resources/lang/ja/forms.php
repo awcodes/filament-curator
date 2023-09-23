61de6f8b1af944b945fc1b1da79614b443732620
kuragehime <kuragehime641@gmail.com>
2023-09-24 00:11:16 +0900

    Create forms.php

---
+++ resources/lang/ja/forms.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'sections' => [
+        'details' => '詳細',
+        'exif' => 'Exifデータ',
+        'file' => 'ファイル',
+        'meta' => 'Meta',
+        'preview' => 'プレビュー',
+        'upload_new' => '新規アップロード',
+        'upload_new_helper' => 'このメディアのキュレーションがある場合は、再作成が必要になる可能性がありますが、自動的行われることはません。',
+        'curation' => 'キュレーション',
+    ],
+    'fields' => [
+        'alt' => '代替テキスト',
+        'alt_hint' => '代替？',
+        'caption' => 'キャプション',
+        'description' => '説明',
+        'file' => 'ファイル',
+        'title' => 'タイトル',
+        'name' => '名前',
+    ],
+    'notices' => [
+        'success' => 'アイテムは正常に更新されました。',
+    ],
+    'curations' => [
+        'button_label' => 'キュレーションを作成',
+    ],
+];
