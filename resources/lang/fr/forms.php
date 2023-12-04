fb704778ddda742021ae0a0056c38c92b3215d50
Voltra <Voltra@users.noreply.github.com>
2023-12-04 20:54:13 +0000

    Add french translations

---
+++ resources/lang/fr/forms.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'sections' => [
+        'details' => 'Détails',
+        'exif' => 'Données EXIF',
+        'file' => 'Fichier',
+        'meta' => 'Meta',
+        'preview' => 'Prévisualiser',
+        'upload_new' => 'Téléverser',
+        'upload_new_helper' => 'Si des styles d\'images ont été générés, il est possible qu\'il faille les regénérer.',
+        'curation' => 'Styles d\'images',
+    ],
+    'fields' => [
+        'alt' => 'Alt',
+        'alt_hint' => 'Alt ?',
+        'caption' => 'Légende',
+        'description' => 'Description',
+        'file' => 'Fichier',
+        'title' => 'Titre',
+        'name' => 'Nom',
+    ],
+    'notices' => [
+        'success' => 'Média mis à jour avec succès.',
+    ],
+    'curations' => [
+        'button_label' => 'Créer un style d\'images',
+    ],
+    'multi_upload' => [
+        'action_label' => 'Téléverser',
+        'modal_heading' => 'Téléverser plusieurs fichiers',
+        'modal_file_label' => 'Fichier',
+    ],
+];
