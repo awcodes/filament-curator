f3da97d01d5d5369ea0704b7ddbac48dae2ab5e6
Adam Weston <awcodes1@gmail.com>
2024-01-07 09:48:16 -0500

    Chore: upgrade glide

--- src/Glide/DefaultServerFactory.php
+++ src/Glide/DefaultServerFactory.php
@@ -2,7 +2,7 @@
 
 namespace Awcodes\Curator\Glide;
 
-use League\Glide\Responses\LaravelResponseFactory;
+use League\Glide\Responses\SymfonyResponseFactory;
 use League\Glide\Server;
 use League\Glide\ServerFactory;
 
@@ -12,7 +12,7 @@ class DefaultServerFactory implements Contracts\ServerFactory
     {
         return ServerFactory::create([
             'driver' => 'gd',
-            'response' => new LaravelResponseFactory(app('request')),
+            'response' => new SymfonyResponseFactory(app('request')),
             'source' => storage_path('app'),
             'source_path_prefix' => 'public',
             'cache' => storage_path('app'),
