eedab39c0463d7c2df10915d631ac5c7f303f3c4
encoderuz <abduqodirxojiyev@gmail.com>
2024-01-25 18:20:03 +0500

    Uzbek language translations added successfully!

---
+++ resources/lang/uz/forms.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'sections' => [
+        'details' => 'Tafsilotlar',
+        'exif' => 'Almashtiriladigan rasm ma\'lumoti',
+        'file' => 'Fayl',
+        'meta' => 'Meta ma\'lumotlar',
+        'preview' => 'Ko\'rish',
+        'upload_new' => 'Yangi yuklash',
+        'upload_new_helper' => 'Agar siz ushu rasmni almashtirmoqchi bo\'lsangiz bu yerdan yuklashingiz mumkin. Eslatma: yangi fayl yuklangandan keyin oldingi fayl o\'chib ketadi',
+        'curation' => 'Tanlanadigan rasm',
+    ],
+    'fields' => [
+        'alt' => 'Alternativ matn',
+        'alt_hint' => 'Alternativ matn nima?',
+        'caption' => 'Subtitr',
+        'description' => 'Tafsilot',
+        'file' => 'Fayl',
+        'title' => 'Sarlavha',
+        'name' => 'Nomi',
+    ],
+    'notices' => [
+        'success' => 'Element muvaffaqiyatli yangilandi.',
+    ],
+    'curations' => [
+        'button_label' => 'Tanlanuvchi rasm yaratish',
+    ],
+    'multi_upload' => [
+        'action_label' => 'Ko\'p miqdorda yuklash',
+        'modal_heading' => 'Ko\'p miqdorda rasm yuklash',
+        'modal_file_label' => 'Fayllar',
+    ],
+];
