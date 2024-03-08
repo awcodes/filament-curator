443a54ad09f79bf7dfd76c3199399fe8e41e38bb
M.Nhat <dmnhat@vlab.tech>
2024-03-08 13:52:46 +0700

    add vn language

---
+++ resources/lang/vi/forms.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'sections' => [
+        'details' => 'Chi tiết',
+        'exif' => 'Định dạng tập tin',
+        'file' => 'Tệp tin',
+        'meta' => 'Nội dung',
+        'preview' => 'Xem trước',
+        'upload_new' => 'Tập tin mới',
+        'upload_new_helper' => 'Nếu bạn có bất kỳ chỉnh sửa nào trên tệp tin cũ, bạn phải tạo lại tệp tin đó, những tệp tin chỉnh sửa sẽ không tự động cập nhật!',
+        'curation' => 'Chỉnh sửa',
+    ],
+    'fields' => [
+        'alt' => 'Văn bản thay thế',
+        'alt_hint' => 'Xem?',
+        'caption' => 'Đầu đề',
+        'description' => 'Miêu tả',
+        'file' => 'Tệp tin',
+        'title' => 'Tiêu đề',
+        'name' => 'Tên tệp tin',
+    ],
+    'notices' => [
+        'success' => 'Cập nhật tệp tin thành công.',
+    ],
+    'curations' => [
+        'button_label' => 'Tạo tệp chỉnh sửa',
+    ],
+    'multi_upload' => [
+        'action_label' => 'Đăng tải nhiều tệp tin',
+        'modal_heading' => 'Đăng tải nhiều tệp tin',
+        'modal_file_label' => 'Các tệp tin',
+    ],
+];
