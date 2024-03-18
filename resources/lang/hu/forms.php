ee5f192575dfa9c04caf6cbe9e49e6262c1e65dc
onlyoff <paladianakos@gmail.com>
2024-03-18 17:44:09 +0100

    add hu language
    Add Hungarian language

---
+++ resources/lang/hu/forms.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'sections' => [
+        'details' => 'Részletek',
+        'exif' => 'Exif adatok',
+        'file' => 'Fájl',
+        'meta' => 'Meta',
+        'preview' => 'Előnézet',
+        'upload_new' => 'Új feltöltés',
+        'upload_new_helper' => 'Ha van bármilyen kurációd ehhez a médiához, akkor azokat valószínűleg újra létre kell majd hoznod, ez nem fog automatikusan megtörténni.',
+        'curation' => 'Kuráció',
+    ],
+    'fields' => [
+        'alt' => 'Alternatív  szöveg',
+        'alt_hint' => 'Alt?',
+        'caption' => 'Felirat',
+        'description' => 'Leírás',
+        'file' => 'Fájl',
+        'title' => 'Cím',
+        'name' => 'Név',
+    ],
+    'notices' => [
+        'success' => 'Az elem sikeresen szerkesztve.',
+    ],
+    'curations' => [
+        'button_label' => 'Kuráció létrehozása',
+    ],
+    'multi_upload' => [
+        'action_label' => 'Csoportos feltöltése',
+        'modal_heading' => 'Több fájl feltöltése',
+        'modal_file_label' => 'Fájlok',
+    ],
+];
