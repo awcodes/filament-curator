9366f407c164b77b2149a6bf1152a9a537c25477
corean <corean@corean.biz>
2024-04-04 23:53:15 +0900

    - Korean translations add

---
+++ resources/lang/ko/forms.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'sections' => [
+        'details' => '세부 정보',
+        'exif' => 'Exif 데이터',
+        'file' => '파일',
+        'meta' => '메타',
+        'preview' => '미리보기',
+        'upload_new' => '새로 업로드',
+        'upload_new_helper' => '이미지에 대한 수정 사항이 있다면 재생성해야 할 수 있습니다. 자동으로 이루어지지 않습니다.',
+        'curation' => '큐레이션',
+    ],
+    'fields' => [
+        'alt' => '대체 텍스트',
+        'alt_hint' => '대체 텍스트 입력',
+        'caption' => '캡션',
+        'description' => '설명',
+        'file' => '파일',
+        'title' => '제목',
+        'name' => '이름',
+    ],
+    'notices' => [
+        'success' => '항목이 성공적으로 업데이트되었습니다.',
+    ],
+    'curations' => [
+        'button_label' => '큐레이션 생성',
+    ],
+    'multi_upload' => [
+        'action_label' => '여러 파일 업로드',
+        'modal_heading' => '여러 파일 업로드',
+        'modal_file_label' => '파일',
+    ],
+];
