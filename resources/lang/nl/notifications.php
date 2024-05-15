e482bfe1305740c248949a96234a4cd0ea691ece
Duncan <duncan.pronk@wearejust.com>
2024-05-15 09:28:19 +0200

    feat: added Dutch translations

---
+++ resources/lang/nl/notifications.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'update_success' => 'Het record is succesvol bijgewerkt.',
+    'update_error' => 'Er is een fout opgetreden bij het bijwerken van de record.',
+    'delete_success' => 'Het record is succesvol verwijderd.',
+    'delete_error' => 'Er is een fout opgetreden bij het verwijderen van de record.',
+];
