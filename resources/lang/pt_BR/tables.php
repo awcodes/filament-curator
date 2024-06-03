5c365782be20d05e17081d14edd84fbd1c1a5d36
Aislan Dener Souza Vicentini <aislandenersouzavicentini@gmail.com>
2024-06-03 16:32:39 -0300

    Add pt_BR support

---
+++ resources/lang/pt_BR/tables.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'columns' => [
+        'url' => 'URL',
+        'name' => 'Nome',
+        'ext' => 'Extensão',
+        'disk' => 'Disco',
+        'directory' => 'Diretório',
+        'created_at' => 'Criado em',
+    ],
+    'actions' => [
+        'toggle_table_grid' => 'Ver em grade',
+        'toggle_table_list' => 'Ver em Lista',
+    ],
+];
