9626486d46f44508c096e647efc8ce5742e362d7
Halil Çağrı Akkuzu <halilcagriakkuzu@gmail.com>
2024-06-17 14:31:56 +0300

    Add translations for `TR`-`Turkish`

---
+++ resources/lang/tr/notifications.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'update_success' => 'Kayıt başarıyla güncellendi.',
+    'update_error' => 'Kayıt güncellenirken bir hata oluştu.',
+    'delete_success' => 'Kayıt başarıyla silindi.',
+    'delete_error' => 'Kayıt silinirken bir hata oluştu.',
+];
