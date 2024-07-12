16e63d269b13986f58296b6542c1fda142d65438
GeorgeFourkas <giorgosfourkas.98@gmail.com>
2024-07-12 20:48:40 +0300

    Added Greek Localization

---
+++ resources/lang/el/tables.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'columns' => [
+        'url' => 'Σύνδεσμος',
+        'name' => 'Όνομα',
+        'ext' => 'Επέκταση',
+        'disk' => 'Δίσκος',
+        'directory' => 'Φάκελος',
+        'created_at' => 'Ημερομηνία Δημιουργίας',
+    ],
+    'actions' => [
+        'toggle_table_grid' => 'Προβολή ως Λίστα',
+        'toggle_table_list' => 'Προβολή ως Πλέγμα',
+    ],
+];
