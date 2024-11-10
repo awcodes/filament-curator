a70e1657fda0aa191f19487b480cc96859395781
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-11-10 02:54:56 +0100

    Polish language support added

---
+++ resources/lang/pl/notifications.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'update_success' => 'Rekord został pomyślnie zaktualizowany.',
+    'update_error' => 'Wystąpił błąd podczas aktualizacji rekordu.',
+    'delete_success' => 'Rekord został pomyślnie usunięty.',
+    'delete_error' => 'Wystąpił błąd podczas usuwania rekordu.',
+];
---
+++ resources/lang/pl/tables.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'columns' => [
+        'url' => 'URL',
+        'name' => 'Nazwa',
+        'ext' => 'Rozszerzenie',
+        'disk' => 'Dysk',
+        'directory' => 'Katalog',
+        'created_at' => 'Utworzono',
+    ],
+    'actions' => [
+        'toggle_table_grid' => 'Widok siatki',
+        'toggle_table_list' => 'Widok listy',
+    ],
+];
