c672a20e9c295e6850b3648c41db615561878b82
Adam Weston <adam@awcodes.com>
2025-12-22 11:27:02 -0500

    remove strictBooleans from rector

--- rector.php
+++ rector.php
@@ -14,6 +14,5 @@ return RectorConfig::configure()
         typeDeclarations: true,
         privatization: true,
         earlyReturn: true,
-        strictBooleans: true,
     )
     ->withPhpSets();
